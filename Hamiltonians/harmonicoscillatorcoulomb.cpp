b63366eb1b4586a1038ce54fc3d05a7f8605bfd5
david <david.svejda2000@gmail.com>
2023-04-28 12:00:25 +0200

    Added HO with coulomb interaction

---
+++ Hamiltonians/harmonicoscillatorcoulomb.cpp
@@ -0,0 +1,53 @@
+#include <cassert>
+#include <iostream>
+#include <math.h>
+#include <memory>
+
+#include "../WaveFunctions/wavefunction.h"
+#include "../particle.h"
+#include "harmonicoscillatorcoulomb.h"
+
+using std::cout;
+using std::endl;
+
+// No interaction
+// Spherical geometry
+
+HormonicOscillatorCoulomb::HormonicOscillatorCoulomb(double omega)
+{
+    assert(omega > 0);
+    m_omega = omega;
+}
+
+double HormonicOscillatorCoulomb::computeLocalEnergy(
+    class WaveFunction &waveFunction,
+    std::vector<std::unique_ptr<class Particle>> &particles)
+{
+    double r2 = 0;
+    unsigned int N = particles.size();
+    std::vector<double> position, position2;
+    for (unsigned int i = 0; i < N; i++)
+    {
+        position = particles[i]->getPosition();
+        for (unsigned int i = 0; i < particles[0]->getNumberOfDimensions(); i++)
+            r2 += position[i] * position[i];
+    }
+    // m = omega = 1
+    double potentialEnergy = 0.5 * r2;
+    double kineticEnergy = waveFunction.computeDoubleDerivative(particles) * -0.5;
+    unsigned int N = particles.size();
+    double interactionEnergy = 0;
+    for (unsigned int i = 0; i < N; i++)
+    {
+        position = particles[i]->getPosition();
+        for (unsigned int j = 0; j < i; i++)
+        {
+            position2 = particles[j]->getPosition();
+            r2 = 0;
+            for (unsigned int k = 0; k < particles[0]->getNumberOfDimensions(); k++)
+                r2 += (position[k] - position2[k]) * (position[k] - position2[k]);
+            interactionEnergy += 1 / std::sqrt(r2);
+        }
+    }
+    return (kineticEnergy + potentialEnergy + interactionEnergy) / N;
+}
---
+++ Hamiltonians/harmonicoscillatorcoulomb.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <memory>
+#include <vector>
+
+#include "hamiltonian.h"
+
+class HormonicOscillatorCoulomb : public Hamiltonian
+{
+public:
+    HormonicOscillatorCoulomb(double omega);
+    double computeLocalEnergy(
+        class WaveFunction &waveFunction,
+        std::vector<std::unique_ptr<class Particle>> &particles);
+
+private:
+    double m_omega;
+};
